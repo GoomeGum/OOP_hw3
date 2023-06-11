a95f6ead894d22c3282340b5fb298cd915d6a3c5
GoomeGum <130835329+GoomeGum@users.noreply.github.com>
2023-06-11 10:06:49 -0700

    stuff

--- src/Tile/Unit/Players/Warrior/AvengerShiled.java
+++ src/Tile/Unit/Players/Warrior/AvengerShiled.java
@@ -1,16 +1,16 @@
 package Tile.Unit.Players.Warrior;
 
 import Tile.Unit.Health;
-import Tile.Unit.Players.Interfaces.IAbility;
 import Tile.Unit.Players.Modifiers;
 import Tile.Unit.Players.Player;
 
-public class AvengerShiled implements IAbility {
+public class AvengerShiled{
     private int _hitPower;
     private int _heal;
     private int _abilityCoolDown;
     private int _remainingCoolDown;
-    AvengerShiled(int hitPower,int heal,int abilityCoolDown){
+
+    AvengerShiled(int hitPower, int heal, int abilityCoolDown) {
         _heal = heal;
         _abilityCoolDown = abilityCoolDown;
         _hitPower = hitPower;
@@ -20,15 +20,17 @@ public class AvengerShiled implements IAbility {
     public int getHeal() {
         return _heal;
     }
-    @Override
-    public void gameTick(){
-        if(_remainingCoolDown > 0)
+
+    public void gameTick() {
+        if (_remainingCoolDown > 0)
             _remainingCoolDown--;
     }
-    public void abilityCast(Health health, int defense, Player player){
+
+
+    public void abilityCast(Health health, int defense, Player player) {
         _remainingCoolDown = _abilityCoolDown;
-        health.set_healthAmount(Math.min(health.getHealthAmount()+defense* Modifiers.WorriorAbilityHealthModifier,health.get_healthPool()));
-        if(player ==null)
+        health.set_healthAmount(Math.min(health.getHealthAmount() + defense * Modifiers.WorriorAbilityHealthModifier, health.get_healthPool()));
+        if (player == null)
             return;
         player.defend();
     }
