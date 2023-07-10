fff5a09a2db7ef8eb14e56ddae90123315ea667c
omer gonen <omergonen13@gmail.com>
2023-07-10 23:28:02 +0300

    forget to add wall

---
+++ src/Playable/Wall.java
@@ -0,0 +1,12 @@
+package Playable;
+
+public class Wall extends Tile {
+    public Wall(char tile, Position position) {
+        super(tile, position);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
